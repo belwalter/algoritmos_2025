5c67fc5624b5fa7013840b0b1d1136c49dc6b806
belwalter <belwalterv@gmail.com>
2025-05-26 19:55:08 -0300

    add example list of list

--- prueba_list.py
+++ prueba_list.py
@@ -43,6 +43,14 @@ from list_ import List
 # list_people.show()
 
 from super_heroes_data import superheroes
+
+def order_by_name(item):
+    return item.name
+
+def order_by_year(item):
+    return item.year
+
+
 class Superhero:
     
     def __init__(self, name, alias, real_name, short_bio, first_appearance, is_villain):
@@ -52,15 +60,30 @@ class Superhero:
         self.short_bio = short_bio
         self.first_appearance = first_appearance
         self.is_villain = is_villain
+        self.movies = List()
+        self.movies.add_criterion('name', order_by_name)
+        self.movies.add_criterion('year', order_by_year)
 
     def __str__(self):
         return f"{self.name}, {self.real_name} - {self.is_villain}"
 
-def order_by_name(item):
-    return item.name
+
+class Movie:
+
+    def __init__(self, name, year):
+        self.name = name
+        self.year = year
+    
+    def __str__(self):
+        return f"{self.name} - {self.year}"
+
+
 
 list_superhero = List()
 list_superhero.add_criterion('name', order_by_name)
+
+# list_movie = List()
+# list_movie.add_criterion('name', order_by_name)
 
 
 for superhero in superheroes:
@@ -106,4 +129,40 @@ print()
 for superhero in list_superhero:
     if superhero.name.startswith(('Ba', 'Cap', 'Spi', 'Y')):
         print(superhero)
+
+list_superhero.show()
+
+movie = Movie('Iron Man I', 2008)
+movie2 = Movie('Iron Man II', 2010)
+movie3 = Movie('Avengers', 2012)
+movie4 = Movie('Civil War', 2016)
+
+
+pos = list_superhero.search('Iron Man', 'name')
+if pos is not None:
+    print(list_superhero[pos])
+    print()
+    hero = list_superhero[pos]
+    hero.movies.append(movie)
+    hero.movies.append(movie2)
+    hero.movies.append(movie3)
+    hero.movies.append(movie4)
+    # hero.movies.sort_by_criterion('name')
+    # hero.movies.show()
+    # print(list_superhero[pos])
+
+    # pos_movie = hero.movies.search('Civil War', 'name')
+    # if pos_movie is not None:
+    #     print(hero.movies[pos_movie])
+    
+    # pos_movie = hero.movies.search('Iron Man I', 'name')
+    # if pos_movie is not None:
+    #     hero.movies[pos_movie].year = 2006
+    pos_movie = hero.movies.search(2016, 'year')
+    if pos_movie is not None:
+        print(f"pelicula eliminada {hero.movies.delete_value(2016, 'year')}")
+
+    print()
+
+
 
