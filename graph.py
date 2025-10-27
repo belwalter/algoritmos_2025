d4787ae919e03a731f0a77f9cd71122147309081
Walter Bel <belwalterv@gmail.com>
2025-10-27 20:30:01 -0300

    add more methods

--- graph.py
+++ graph.py
@@ -2,14 +2,17 @@ from typing import Any, Optional
 
 from list_ import List
 
-class Graph(list):
+class Graph(List):
 
     class __nodeVertex:
 
         def __init__(self, value: Any, other_values: Optional[Any] = None):
             self.value = value
             self.edges = List()
-            self.other_values = other_values # no esta en uso aun
+            self.edges.add_criterion('value', Graph._order_by_value)
+            self.edges.add_criterion('weight', Graph._order_by_weight)
+            self.other_values = other_values
+            self.visited = False
         
         def __str__(self):
             return self.value
@@ -24,22 +27,16 @@ class Graph(list):
         def __str__(self):
             return f'Destination: {self.value} with weight --> {self.weight}'
     
-    CRITERION_FUNCTIONS = {}
-    
-    def __init__(self, is_directed=True):
-        def order_by_value(item):
-            return item.value
-        
-        self.add_criterion('value', order_by_value)
+    def __init__(self, is_directed=False):
+        self.add_criterion('value', self._order_by_value)
         self.is_directed = is_directed
 
-    def add_criterion(
-        self,
-        key_criterion: str,
-        function,
-    ):
-        self.CRITERION_FUNCTIONS[key_criterion] = function
+    def _order_by_value(item):
+        return item.value
 
+    def _order_by_weight(item):
+        return item.weight
+    
     def show(
         self
     ) -> None:
@@ -55,64 +52,66 @@ class Graph(list):
         self.append(node_vertex)
 
     def insert_edge(self, origin_vertex: Any, destination_vertex: Any, weight: int) -> None:
-        origin = self.search(origin_vertex)
-        destination = self.search(destination_vertex)
+        origin = self.search(origin_vertex, 'value')
+        destination = self.search(destination_vertex, 'value')
         if origin is not None and destination is not None:
             node_edge = Graph.__nodeEdge(destination_vertex, weight)
             self[origin].edges.append(node_edge)
-            if not self.is_directed and origin != destination:
+            if self.is_directed and origin != destination:
                 node_edge = Graph.__nodeEdge(origin_vertex, weight)
                 self[destination].edges.append(node_edge)
         else:
             print('no se puede insertar falta uno de los vertices')
 
-    # def delete_value(
-    #     self,
-    #     value,
-    #     key_value: str = None,
-    # ) -> Optional[Any]:
-    #     index = self.search(value, key_value)
-    #     return self.pop(index) if index is not None else index
-
-    def sort_by_criterion(
+    def delete_edge(
         self,
-        criterion_key: str = None,
-    ) -> None:
-        criterion = self.CRITERION_FUNCTIONS.get(criterion_key)
+        origin,
+        destination,
+        key_value: str = None,
+    ) -> Optional[Any]:
+        pos_origin = self.search(origin, key_value)
+        if pos_origin is not None:
+            edge = self[pos_origin].edges.delete_value(destination, key_value)
+            if self.is_directed and edge is not None:
+                pos_destination = self.search(destination, key_value)
+                if pos_destination is not None:
+                    self[pos_destination].edges.delete_value(origin, key_value)
+            return edge
 
-        if criterion is not None:
-            self.sort(key=criterion)
-        elif self and  isinstance(self[0], (int, str, bool)):
-            self.sort()
-        else:
-            print('criterio de orden no encontrado')
-
-    def search(
+    def delete_vertex(
         self,
-        search_value,
-        search_key: str = 'value',
-    ) -> int:
-        self.sort_by_criterion(search_key)
-        start = 0
-        end = len(self) -1
-        middle = (start + end) // 2
+        value,
+        key_value_vertex: str = None,
+        key_value_edges: str = 'value',
+    ) -> Optional[Any]:
+        delete_value = g.delete_value(value, key_value_vertex)
+        if delete_value is not None:
+            for vertex in self:
+                self.delete_edge(vertex.value, value, key_value_edges)
+        return delete_value
 
-        while start <= end:
-            criterion = self.CRITERION_FUNCTIONS.get(search_key)
-            if criterion is None and self and not isinstance(self[0], (int, str, bool)):
-                return None
+    def mark_as_unvisited(self) -> None:
+        for vertex in self:
+            vertex.visited = False
 
-            value = criterion(self[middle]) if criterion else self[middle]
-            if value == search_value:
-                return middle
-            elif value  < search_value:
-                start = middle +1
-            else:
-                end = middle -1
-            middle = (start + end) // 2
+    def deep_sweep(self, value) -> None:
+        def __deep_sweep(graph, value):
+            vertex_pos = graph.search(value, 'value')
+            if vertex_pos is not None:
+                if not graph[vertex_pos].visited:
+                    graph[vertex_pos].visited = True
+                    print(graph[vertex_pos])
+                    for edge in graph[vertex_pos].edges:
+                        destination_edge_pos = graph.search(edge.value, 'value')
+                        if not graph[destination_edge_pos].visited:
+                            __deep_sweep(graph, graph[destination_edge_pos].value)
+
+        self.mark_as_unvisited()
+        __deep_sweep(self, value)
+        
 
 
-g = Graph(is_directed=False)
+g = Graph()
 
 g.insert_vertex('A')
 g.insert_vertex('I')
@@ -122,9 +121,37 @@ g.insert_vertex('G')
 
 g.insert_edge('A', 'Z', 14)
 g.insert_edge('A', 'G', 4)
+g.insert_edge('I', 'A', 20)
+g.insert_edge('A', 'B', 7)
 g.insert_edge('B', 'Z', 144)
-g.insert_edge('B', 'I', 40)
-g.insert_edge('I', 'I', 24)
-g.insert_edge('I', 'Z', 11)
+g.insert_edge('B', 'A', 40)
+g.insert_edge('G', 'A', 24)
+# g.insert_edge('B', 'I', 11)
 
 g.show()
+print()
+
+# vertex = g.delete_vertex('A', 'value')
+# print(f'deleted vertex: {vertex}')
+
+g.deep_sweep('A')
+
+# print()
+# for vertex in g:
+#     print(vertex.value, vertex.visited)
+# g.show()
+# print('segundo barrido')
+# g.deep_sweep('I')
+
+# es_adyacente(vértice, destino). Devuelve verdadero (true) si el destino es un nodo adyacente
+# al vértice;
+# adyacentes(vértice). Realiza un barrido de los nodos adyacentes al vértice;
+
+# existe _paso(grafo, vértice origen, vértice destino). Devuelve verdadero (true) si es posible ir des-
+# de el vértice origen hasta el vértice destino, caso contrario retornará falso (false);
+
+# barrido_profundidad(grafo, vértice inicio). Realiza un barrido en profundidad del grafo a par-
+# tir del vértice de inicio;
+
+# barrido_amplitud(grafo, vértice inicio). Realiza un barrido en amplitud del grafo a partir del
+# vértice de inicio;
